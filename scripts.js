9be4f9cd933e74fbb5199bb9cbc1cb85042e3255
HarryStevens <harryjosephstevens@gmail.com>
2014-04-08 12:16:09 -0400

    Files working

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
 /**
  * @author Harry Stevens
  */
+
+function modals() {
+
+	$('.btn-cyclone').on("click", function() {
+		$('#cycloneModal').modal();
+	});
+	$('.btn-wheel').on("click", function() {
+		$('#wheelModal').modal();
+	});
+	$('.btn-para').on("click", function() {
+		$('#paraModal').modal();
+	});
+}
+
+
+$(document).ready(function() {
+	modals();
+});
